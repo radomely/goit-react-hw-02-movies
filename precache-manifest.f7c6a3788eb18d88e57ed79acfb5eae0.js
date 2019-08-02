db42dc25afb255b6ea94c71112f572bb079d2951
Andrii <radomely@gmail.com>
2019-08-02 22:05:49 +0300

    Updates

---
+++ precache-manifest.f7c6a3788eb18d88e57ed79acfb5eae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfa092a68e3384d0e88f98d62aa9e6c",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "a9adcd75b615bdb1c5a1",
+    "url": "/goit-react-hw-02-movies/static/css/main.a02fff47.chunk.css"
+  },
+  {
+    "revision": "5b417f6a74b517fd518a",
+    "url": "/goit-react-hw-02-movies/static/js/2.22fdb382.chunk.js"
+  },
+  {
+    "revision": "a9adcd75b615bdb1c5a1",
+    "url": "/goit-react-hw-02-movies/static/js/main.dde947ae.chunk.js"
+  },
+  {
+    "revision": "c0f0bf3555f23a72488d",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.4f93631a.js"
+  }
+]);
